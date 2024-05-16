4fdc68a62d8d94a89c21fbefacc5fb2d9f7abaea
timostadelmayer <masztalerztimo@gmail.com>
2024-05-16 09:16:22 +0200

    !5.05. name AGE

--- app/src/main/java/com/example/myapplication/PersonFragment.kt
+++ app/src/main/java/com/example/myapplication/PersonFragment.kt
@@ -2,32 +2,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import com.example.myapplication.databinding.FragmentHomeBinding
+import com.example.myapplication.databinding.FragmentPersonBinding
 
-class HomeFragment : Fragment() {
-    private lateinit var binding: FragmentHomeBinding
+class PersonFragment : Fragment() {
+    private lateinit var binding: FragmentPersonBinding
     private val sharedViewModel: SharedViewModel by activityViewModels()
-
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        binding = FragmentPersonBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.nameInput.addTextChangedListener { text ->
-            sharedViewModel.updateData(text.toString())
+        sharedViewModel.Daten.observe(viewLifecycleOwner) {
+            binding.personNameTextView.text = it
         }
-        binding.ageInput.addTextChangedListener { text ->
-            sharedViewModel.updateData(text.toString())
+        sharedViewModel.Daten.observe(viewLifecycleOwner) {binding.personAgeTextView.text=it}
     }
-}}
+}
